a9336a62d4dd76f6361fbbc905351902844045e0
James Lee Scott <me@jamesscott.pw>
2018-12-29 14:20:58 +0000

    system: login locked files

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
-
+  require 'steamauth/steamauth.php';
+  if(!isset($_SESSION['steamid'])) {
+    $logged = false;
+  } else {
+    $logged = true;
+    include ('steamauth/userInfo.php');
+  }
   $request = explode("/",strtolower($_SERVER['REQUEST_URI']));
 
   // Pages locked to login
-  $locked_pages = Array('index');
+  $locked_pages = Array('home');
 
   // Request variables
   $request_page = $request[1];
@@ -11,14 +17,19 @@
   $request_arg2 = $request[3];
   $request_arg3 = $request[4];
 
-  if (!$request_page or $request_page == '' or $request_page == 'index') {
+  if (!$request_page or $request_page == '' or $request_page == 'index 'or $request_page == 'logout') {
     $request_page = 'home';
   }
 
   if (in_array($request_page, $locked_pages)) {
-    pass;
+    if ($logged) {
+      include('templates/pages/'.$request_page.'.php');
+    } else {
+      header('Location: http://dashboard.celerium.co/login');
+    }
   } else {
     if (!file_exists('templates/pages/'.$request_page.'.php')) {
+      //header('Location: http://dashboard.celerium.co/404');
       include('templates/pages/404.php');
     } else {
       include('templates/pages/'.$request_page.'.php');
