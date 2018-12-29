0eb2a4a0b96b1afde9fe6b0022e54059fc3d6bdd
James Lee Scott <me@jamesscott.pw>
2018-12-29 14:03:07 +0000

    steamauth: Update settings

--- steamauth/SteamConfig.php
+++ steamauth/SteamConfig.php
@@ -1,9 +1,9 @@
 <?php
 //Version 4.0
-$steamauth['apikey'] = ""; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
-$steamauth['domainname'] = ""; // The main URL of your website displayed in the login page
-$steamauth['logoutpage'] = ""; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
-$steamauth['loginpage'] = ""; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['apikey'] = "53345E4CA3FBA26A7AAFC4D6285F4BD8"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
+$steamauth['domainname'] = "dashboard.celerium.co"; // The main URL of your website displayed in the login page
+$steamauth['logoutpage'] = "logout"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['loginpage'] = "home"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
 // System stuff
 if (empty($steamauth['apikey'])) {die("<div style='display: block; width: 100%; background-color: red; text-align: center;'>SteamAuth:<br>Please supply an API-Key!<br>Find this in steamauth/SteamConfig.php, Find the '<b>\$steamauth['apikey']</b>' Array. </div>");}
