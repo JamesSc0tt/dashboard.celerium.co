20c929d0c2085fb5e311911b71a6d0f51c44c580
James Lee Scott <me@jamesscott.pw>
2019-01-05 13:12:29 +0000

    external links update

--- templates/header.php
+++ templates/header.php
@@ -4,38 +4,38 @@
 <div id="user-nav" class="navbar navbar-inverse">
   <ul class="nav">
     <li class="" ><a title="" href="#" style="padding: 9px 10px 7px 10px;"><img src="<?php echo $steamprofile['avatar'] ?>" style="border-radius:50%;margin-right:5px;"> <span class="text"><?php echo $steamprofile['personaname']?></span></a></li>
-    <li class=""><a title="" href="/steamauth/steamauth.php?logout"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a></li>
+    <li class=""><a title="" href="https://dashboard.celerium.co/steamauth/steamauth.php?logout"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a></li>
   </ul>
 </div>
 <div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Home</a><ul>
     <li class="active"><a href="home"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
     <li class="submenu"> <a href="home"><i class="icon icon-file"></i> <span>Applications</span> <span class="label label-important">4</span></a>
       <ul>
-        <li><a href="applications">My Applications</a></li>
-        <li><a href="applications/new">New Application</a></li>
+        <li><a href="https://dashboard.celerium.co/applications">My Applications</a></li>
+        <li><a href="https://dashboard.celerium.co/applications/new">New Application</a></li>
         <li style="height:25px;background-color:#fff;font-size:14px;line-height:25px;border:1px solid #49cced;"><b style="padding-left:10px;color:#49cced;">ADMIN OPTIONS</b></li>
-        <li><a href="applications/moderation">Awaiting Moderation</a></li>
-        <li><a href="applications/review">Awaiting Review</a></li>
-        <li><a href="applications/all">All Applications</a></li>
+        <li><a href="https://dashboard.celerium.co/applications/moderation">Awaiting Moderation</a></li>
+        <li><a href="https://dashboard.celerium.co/applications/review">Awaiting Review</a></li>
+        <li><a href="https://dashboard.celerium.co/applications/all">All Applications</a></li>
       </ul>
     </li>
     <li> <a href="warnings"><i class="icon icon-book"></i> <span>Warnings</span></a> </li>
     <li> <a href="bans"><i class="icon icon-calendar"></i> <span>Bans</span></a> </li>
     <li class="submenu"> <a href="home"><i class="icon icon-file"></i> <span>Appeals</span> <span class="label label-important">4</span></a>
       <ul>
-        <li><a href="appeals/warnings">Warning Appeals</a></li>
-        <li><a href="appeals/bans">Ban Appeals</a></li>
+        <li><a href="https://dashboard.celerium.co/appeals/warnings">Warning Appeals</a></li>
+        <li><a href="https://dashboard.celerium.co/appeals/bans">Ban Appeals</a></li>
         <li style="height:25px;background-color:#fff;font-size:14px;line-height:25px;border:1px solid #49cced;"><b style="padding-left:10px;color:#49cced;">ADMIN OPTIONS</b></li>
-        <li><a href="appeals/moderation">Awaiting Moderation</a></li>
-        <li><a href="appeals/review">Awaiting Review</a></li>
-        <li><a href="appeals/all">All Appeals</a></li>
+        <li><a href="https://dashboard.celerium.co/appeals/moderation">Awaiting Moderation</a></li>
+        <li><a href="https://dashboard.celerium.co/appeals/review">Awaiting Review</a></li>
+        <li><a href="https://dashboard.celerium.co/appeals/all">All Appeals</a></li>
       </ul>
     </li>
     <li class="submenu"> <a href="#"><span style="color:#c61313 !important;">Admin</span></a>
       <ul>
-        <li><a href="users/all">Manage Users</a></li>
-        <li><a href="users/mail">Mass Mail (Forum Email)</a></li>
-        <li><a href="system/announcement">Announcement</a></li>
+        <li><a href="https://dashboard.celerium.co/users/all">Manage Users</a></li>
+        <li><a href="https://dashboard.celerium.co/users/mail">Mass Mail (Forum Email)</a></li>
+        <li><a href="https://dashboard.celerium.co/system/announcement">Announcement</a></li>
       </ul>
     </li>
   </ul>
